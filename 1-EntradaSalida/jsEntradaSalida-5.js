9689371a6a3ae683705406ba0a08d3ca892fdf80
Lautaro01 <36265003+Lautaro01@users.noreply.github.com>
2018-02-09 10:35:29 -0300

    Clase 2, ejercicios 5,6
    Varieables, copilar numeros, transformas letra a numero, suma resta, concatenar.

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -1,9 +1,12 @@
 /*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
-{	var nombre;edad 
-    nombre;edad = document.getElementById("elnombre").value
-    Alert(nombre;edad)
-	
+{	
+    var nombre 
+    var edad
+    nombre = document.getElementById("elNombre").value;
+    edad = document.getElementById("laEdad").value;
+   
+    alert("Usted se llama " + nombre + " y tiene " + edad + " años de edad");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,22 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var numerou;
+    var numerod;
+    var resultado;
+
+    numerou = document.getElementById("numeroUno").value; 
+    numerod = document.getElementById("numeroDos").value;
+
+    numerou = parseInt(numerou);
+    numerod = parseInt(numerod); 
+
+    
+    resultado = numerou + numerod;
+
+    alert("El resulado de su suma es: " + resultado);
+
+
 
 }
 
